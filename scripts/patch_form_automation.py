7a69c06e0bc8f5686fe5cdd7f613fed5a3ca25f4
Leahcim12 <30346110+Leahcim12@users.noreply.github.com>
2026-09-06 16:39:08 +0200

    Publish dedicated supplement inventory form

--- scripts/patch_form_automation.py
+++ scripts/patch_form_automation.py
@@ -3,12 +3,14 @@ import re
 
 path=Path("_site/index.html")
 text=path.read_text(encoding="utf-8")
-version="20260906-lac-solution-form1"
+version="20260906-supplement-form1"
 tag=f'<script src="/form-automation.js?v={version}" id="optykerFormAutomationJs"></script>'
 frame_addon=Path("frame-form-addon.js").read_text(encoding="utf-8")
 frame_tag='<script id="optykerWarehouseFrameFormJs">'+frame_addon+'</script>'
 solution_addon=Path("lac-solution-form-addon.js").read_text(encoding="utf-8")
 solution_tag='<script id="optykerWarehouseLacSolutionFormJs">'+solution_addon+'</script>'
+supplement_addon=Path("supplement-form-addon.js").read_text(encoding="utf-8")
+supplement_tag='<script id="optykerWarehouseSupplementFormJs">'+supplement_addon+'</script>'
 
 if 'id="optykerFormAutomationJs"' in text:
     text=re.sub(r'<script[^>]*id="optykerFormAutomationJs"[^>]*></script>',tag,text,count=1)
@@ -28,5 +30,11 @@ else:
     pos=text.rfind("</body>")
     text=(text[:pos]+solution_tag+"\n"+text[pos:]) if pos>=0 else text+"\n"+solution_tag
 
+if 'id="optykerWarehouseSupplementFormJs"' in text:
+    text=re.sub(r'<script[^>]*id="optykerWarehouseSupplementFormJs"[^>]*>[\s\S]*?</script>',supplement_tag,text,count=1)
+else:
+    pos=text.rfind("</body>")
+    text=(text[:pos]+supplement_tag+"\n"+text[pos:]) if pos>=0 else text+"\n"+supplement_tag
+
 path.write_text(text,encoding="utf-8")
-print("Optyker form automation loader OK",version,"frame form + LAC solutions form")
+print("Optyker form automation loader OK",version,"frame form + LAC solutions form + supplement form")
