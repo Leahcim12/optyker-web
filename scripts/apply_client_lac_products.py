48633f09138c37027d9467b08b8e531188eb190e
Leahcim12 <30346110+Leahcim12@users.noreply.github.com>
2026-09-24 00:03:15 +0200

    build(lac): publish LAC tab workspace v2

--- scripts/apply_client_lac_products.py
+++ scripts/apply_client_lac_products.py
@@ -5,7 +5,7 @@ import json, os
 
 ROOT=Path(__file__).resolve().parent.parent
 SITE=ROOT/'_site'
-VERSION='20260923-lac-products1'
+VERSION='20260923-lac-products2'
 CSS='<link id="optykerClientLacProductsCss" rel="stylesheet" href="/client-lac-products.css?v='+VERSION+'">'
 JS='<script id="optykerClientLacProductsJs" defer src="/client-lac-products.js?v='+VERSION+'"></script>'
 
